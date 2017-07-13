38782c62ffd0a4bab358f162cd5c8267b676b7af
humdeef <1985422402@qq.com>
2017-07-13 23:30:35 +0800

    update

---
+++ BeginnerAdmin/datas/nav.js
@@ -0,0 +1,79 @@
+var navs = [{
+    "title": "商品管理",
+    "icon": "fa-cubes",
+    "spread": true,
+    "children": [{
+        "title": "商品列表",
+        "icon": "&#xe641;",
+        "href": "button.html"
+    }, {
+        "title": "商品类别",
+        "icon": "&#xe63c;",
+        "href": "form.html"
+    }]
+}, {
+    "title": "套餐管理",
+    "icon": "fa-cogs",
+    "spread": false,
+    "children": [
+        {
+            "title": "预定套餐",
+            "icon": "fa-table",
+            "href": "btable.html"
+        },
+        {
+            "title": "团购套餐",
+            "icon": "fa-navicon",
+            "href": "navbar.html"
+        }
+    ]
+}, {
+    "title": "暖场人管理",
+    "icon": "fa-cogs",
+    "spread": false,
+    "href": "button.html",
+    "children": [{
+        "title": "暖场人列表",
+        "icon": "fa-github",
+        "href": "tab.html"
+    }]
+},
+    {
+        "title": "订单管理",
+        "icon": "fa-cogs",
+        "spread": false,
+        "href": "button.html",
+        "children": [{
+            "title": "订单列表",
+            "icon": "&#xe609;",
+            "href": "form.html"
+        }]
+    },
+    {
+        "title": "活动管理",
+        "icon": "fa-cogs",
+        "spread": false,
+        "href": "button.html",
+        "children": [{
+            "title": "活动列表",
+            "icon": "&#xe62a;",
+            "href": "tab.html"
+        }]
+    },
+    {
+        "title": "活动管理",
+        "icon": "fa-stop-circle",
+        "href": "#",
+        "spread": false,
+        "children": [{
+            "title": "活动列表",
+            "icon": "fa-github",
+            "href": "cop.html"
+        }]
+    },
+    {
+        "title": "公司信息管理",
+        "icon": "fa-stop-circle",
+        "href": "https://www.baidu.com",
+        "spread": false
+    }];
